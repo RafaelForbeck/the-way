cbcb3b9cc165a90254f5d6e38f7a45842b6fbd9d
Carlos Rafael Hasselmann Forbeck <rafaelforbeck@gmail.com>
2024-11-25 09:01:36 -0300

    refactor: remove unused script

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var anim := $AnimatedSprite2D
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	get_tree().call_group("checkpoints", "off")
 	anim.play("on")
 	GameManager.update_respawn_point(position)
