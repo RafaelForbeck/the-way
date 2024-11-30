978ce802949d4df6548f17af38ddc4b60a6d257c
Carlos Rafael Hasselmann Forbeck <rafaelforbeck@gmail.com>
2024-11-30 16:29:31 -0300

    feat: creates the fly_enemy enemy variation

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,7 @@
-extends CharacterBody2D
+class_name Enemy extends CharacterBody2D
+
+func _ready() -> void:
+	print("enemy")
 
 func take_damage():
 	print("enemy died")
---
+++ scripts/fly_enemy.gd
@@ -0,0 +1,5 @@
+class_name FlyEnemy extends Enemy
+
+func _ready() -> void:
+	super._ready()
+	print("fly enemy")
