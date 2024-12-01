1df7c6a4bec36ce9ea75e19f9cc4943625b93df2
Carlos Rafael Hasselmann Forbeck <rafaelforbeck@gmail.com>
2024-12-01 16:25:47 -0300

    feat: creates oscillatory movement for enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,38 @@
 class_name Enemy extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var h_oscillation: bool
+@export var h_range: float
+@export var h_speed: float
+
+@export var v_oscillation: bool
+@export var v_range: float
+@export var v_speed: float
+
+var h_timer := 0.0
+var v_timer := 0.0
+
+var start_position: Vector2
+
+func _ready() -> void:
+	start_position = position
+
+func _physics_process(delta: float) -> void:
+	if h_oscillation:
+		h_timer += delta * h_speed
+		if h_timer > 2 * PI:
+			h_timer -= 2 * PI
+		if h_timer > PI:
+			anim.flip_h = true
+		else:
+			anim.flip_h = false
+			
+		position.x = start_position.x + cos(h_timer) * h_range
+		
+	if v_oscillation:
+		v_timer += delta * v_speed
+		position.y = start_position.y + sin(v_timer) * v_range
+
 func take_damage():
 	print("enemy died")
